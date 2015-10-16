83dcd49e880d0fa6cd09f63b4539c03e64ae39e9
chengqixi <chengqixi@gmail.com>
2015-10-16 16:15:45 -0400

    project set up

---
+++ BuildGrids/Assets/scripts/Grid.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid: MonoBehaviour {
+	public LayerMask unwalkableMask;
+	public Vector2 gridWorldSize;
+	public float nodeRadius;
+	Node[,] grid;
+
+	void OnDrawGizmos(){
+		Gizoms.DrawWireCube (transform.position, new Vector3 (gridWorldSize.x, 1, gridWorldSize.y));
+
+	}
+}
