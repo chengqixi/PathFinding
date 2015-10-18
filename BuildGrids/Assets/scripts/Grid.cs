82062f1e9074f2b55dae1040d6fa4abfd123a2c6
chengqixi <chengqixi@gmail.com>
2015-10-18 18:38:21 -0400

    A*

--- BuildGrids/Assets/scripts/Grid.cs
+++ BuildGrids/Assets/scripts/Grid.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Grid: MonoBehaviour {
 
@@ -32,9 +33,32 @@ public class Grid: MonoBehaviour {
             {
                 Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
                 bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
-                grid[x, y] = new Node(walkable, worldPoint);
+                grid[x, y] = new Node(walkable, worldPoint, x, y);
             }
         }
+    }
+
+    public List<Node> GetNeighbours (Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+
+        for (int x = -1; x<=1; x++)
+        {
+            for(int y = -1; y<= 1; y++)
+            {
+                if (x == 0 && y == 0)
+                    continue;
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeX)
+                {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+
+        return neighbours;
     }
 
     public Node NodeFromWorldPoint(Vector3 worldPosition)
